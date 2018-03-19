2a00bcc686fcd8ce72fb7b4fa0a475f919869b43
Cristina Vílchez <cvilchez@edu.tecnocampus.cat>
2018-03-19 11:55:51 +0100

    Update protractor.conf.js

--- config/protractor.conf.js
+++ config/protractor.conf.js
@@ -6,8 +6,7 @@ require('ts-node/register');
 var helpers = require('./helpers');
 
 exports.config = {
-  baseUrl: 'http://localhost:3000/',
-  seleniumAddress: 'http://localhost:9515',
+  seleniumAddress: 'http://localhost:3030',
 
   /**
    * Use `npm run e2e`
