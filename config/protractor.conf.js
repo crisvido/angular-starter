7d87fb322536f2c037835dcf2618b8ed0f4a6f8a
Cristina Vílchez <cvilchez@edu.tecnocampus.cat>
2018-03-19 10:28:46 +0100

    Update protractor.conf.js

--- config/protractor.conf.js
+++ config/protractor.conf.js
@@ -31,7 +31,7 @@ exports.config = {
 
   capabilities: {
     browserName: 'phantomjs',
-	  'phantomjs.binary.path': './node_modules/phantomjs/bin/phantomjs',
+	  'phantomjs.binary.path': 'node_modules/phantomjs/bin/phantomjs',
     'phantomjs.ghostdriver.cli.args': ['--loglevel=DEBUG']
   },
 
