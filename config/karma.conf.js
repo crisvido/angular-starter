f539d932e96c6b3d2af31b85f93a8570ad16d134
Cristina Vílchez <cvilchez@edu.tecnocampus.cat>
2018-03-19 13:49:53 +0100

    Update karma.conf.js

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -121,7 +121,7 @@ module.exports = function (config) {
      * available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
      */
     browsers: [
-	  'ChromeHeadLess',
+	  'Chrome', 'Chromium',
     ],
 
     /**
