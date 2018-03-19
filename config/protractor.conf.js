7b4bb6dd46cf2e567ff84d61e334ad64dd481612
Cristina Vílchez <cvilchez@edu.tecnocampus.cat>
2018-03-19 13:10:23 +0100

    Update protractor.conf.js

--- config/protractor.conf.js
+++ config/protractor.conf.js
@@ -30,11 +30,13 @@ exports.config = {
   },
 
   capabilities: {
-    browserName: 'phantomjs',
-	  'phantomjs.binary.path': '../node_modules/phantomjs/bin/phantomjs',
-    'phantomjs.ghostdriver.cli.args': ['--loglevel=DEBUG']
-  },
+  browserName: 'chrome',
 
+  chromeOptions: {
+     args: [ "--headless", "--disable-gpu", "--window-size=800,600" ]
+   }
+  }
+  
   onPrepare: function() {
     browser.ignoreSynchronization = true;
   },
