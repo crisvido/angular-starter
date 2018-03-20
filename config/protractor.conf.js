7d00329b0ec12b3ef0249b81cf9c8b6b4dadc8ea
Cristina Vílchez <cvilchez@edu.tecnocampus.cat>
2018-03-20 12:42:29 +0100

    Update protractor.conf.js

--- config/protractor.conf.js
+++ config/protractor.conf.js
@@ -34,8 +34,7 @@ exports.config = {
   browserName: 'chrome',
 
   chromeOptions: {
-    args: [ "--headless", "--disable-gpu", "--window-size=800,600" ],
-    binary: 'node_modules/chromedriver/lib/chromedriver/chromedriver.exe'
+    args: [ "--headless", "--disable-gpu", "--window-size=800,600" ]
    }
   },
   
